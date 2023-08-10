fc37985f330ada5bc8766a81f603619a71343514
Abbas Sarwar <abbassarwar100@gmail.com>
2023-08-10 13:37:24 +0500

    Linters fixes

--- spec/integrations/groups_interface_spec.rb
+++ spec/integrations/groups_interface_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Group', type: :feature do
-    describe 'new' do
-        before(:each) do
-            User.destroy_all
-            Group.destroy_all
-            @user = User.create(name:"Abbas", email:"abbas@gmail.com", password:"abbas123")
-            @group = Group.create(name:'Amazon', icon: Rack::Test::UploadedFile.new('spec/assets/daraz.jpg') )
-            visit new_user_session_path
-            fill_in 'Email', with: 'abbas@gmail.com'
-            fill_in 'Password', with: 'abbas123'
-            click_button 'Log in'
-        end
-        it "should have data" do
-            expect(page).to have_content("Your Catogeries")
-            expect(page).to have_content("Home")
-        end
-        it 'Group should Add New' do
-            click_link 'Add New'
-            expect(page).to have_content('Name')
-            expect(page).to have_content('Add new Catogery')
-            fill_in 'Name', with: 'Amazon'
-            attach_file('Icon', Rails.root + 'spec/assets/daraz.jpg')
-            click_button 'Submit'
-            visit root_path
-            expect(page).to have_content('Amazon')
-            expect(page).to have_content(0)
-        end
+  describe 'new' do
+    before(:each) do
+      User.destroy_all
+      Group.destroy_all
+      @user = User.create(name: 'Abbas', email: 'abbas@gmail.com', password: 'abbas123')
+      @group = Group.create(name: 'Amazon', icon: Rack::Test::UploadedFile.new('spec/assets/daraz.jpg'))
+      visit new_user_session_path
+      fill_in 'Email', with: 'abbas@gmail.com'
+      fill_in 'Password', with: 'abbas123'
+      click_button 'Log in'
     end
+    it 'should have data' do
+      expect(page).to have_content('Your Catogeries')
+      expect(page).to have_content('Home')
+    end
+    it 'Group should Add New' do
+      click_link 'Add New'
+      expect(page).to have_content('Name')
+      expect(page).to have_content('Add new Catogery')
+      fill_in 'Name', with: 'Amazon'
+      attach_file('Icon', "#{Rails.root}spec/assets/daraz.jpg")
+      click_button 'Submit'
+      visit root_path
+      expect(page).to have_content('Amazon')
+      expect(page).to have_content(0)
+    end
+  end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    before(:each) {
-        @user = User.new(name:"Abbas", email:"abbas@gmail.com", password:"txt123")
-    }
-    it 'name should be valid during Registeration' do
-        subject.name = nil
-        expect(subject).to_not be_valid
-    end
-    it 'Email should be valid' do
-        subject.email = 'abbas.com'
-        expect(subject).to_not be_valid
-    end
-    it 'Password should be valud' do
-        subject.password = nil
-        expect(subject).to_not be_valid
-    end
+  before(:each) do
+    @user = User.new(name: 'Abbas', email: 'abbas@gmail.com', password: 'txt123')
+  end
+  it 'name should be valid during Registeration' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'Email should be valid' do
+    subject.email = 'abbas.com'
+    expect(subject).to_not be_valid
+  end
+  it 'Password should be valud' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
